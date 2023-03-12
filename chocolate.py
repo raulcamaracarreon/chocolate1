1afeb5ef1fd2adb2454b04e61a500f93e548652a
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-03-12 17:49:26 -0600

    Update chocolate.py

--- chocolate.py
+++ chocolate.py
@@ -34,6 +34,19 @@ b64 = base64.b64encode(csv.encode()).decode()
 href = f'<a href="data:file/csv;base64,{b64}" download="receta_chocolate_amargo.csv">Descargar receta</a>'
 st.markdown(href, unsafe_allow_html=True)
 
+st.write("Instrucciones:")
+
+st.write("1. Derrite la manteca de cacao en un baño María. Para hacer esto, coloca la manteca de cacao en un recipiente resistente al calor y colócalo sobre una olla con agua hirviendo. Revuelve la manteca de cacao hasta que se derrita completamente.")
+
+st.write("2. Una vez que la manteca de cacao se haya derretido, agrega el cacao en polvo, el azúcar de coco o de caña y la pizca de sal. Revuelve hasta que los ingredientes se hayan mezclado completamente.")
+
+st.write("3. Agrega el brandy y el extracto de vainilla a la mezcla de chocolate. Revuelve hasta que estén bien incorporados.")
+
+st.write("4. Vierte la mezcla de chocolate en un molde para barras de chocolate. Si no tienes un molde para barras, puedes usar un molde de pan forrado con papel encerado.")
+
+st.write("5. Refrigera la barra de chocolate durante al menos 30 minutos, o hasta que se haya endurecido por completo.")
+
+st.write("6 .Desmolda la barra de chocolate y córtala en porciones individuales. Sirve y disfruta de tu deliciosa barra de chocolate amargo con brandy.")
 
 
 

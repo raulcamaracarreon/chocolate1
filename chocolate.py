583aeb39b6911c287b36eb031f472a1a39103de4
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-03-12 18:33:22 -0600

    Update chocolate.py

--- chocolate.py
+++ chocolate.py
@@ -5,14 +5,13 @@ import base64
 def calculate_ingredients(manteca_de_cacao):
     cacao_en_polvo = manteca_de_cacao * 0.25
     azucar = manteca_de_cacao * 0.3
-    brandy_gramos = manteca_de_cacao * 0.02 * 15 * 0.9  # Convertir cucharadas a gramos (1 cucharada = 15 ml, densidad de brandy = 0.9 g/ml)
-    vainilla_gramos = manteca_de_cacao * 0.005 * 4.2  # Convertir cucharaditas a gramos (1 cucharadita = 4.2 gramos)
+    brandy_gramos = manteca_de_cacao * 0.02 * 15 * 0.9  
+    vainilla_gramos = manteca_de_cacao * 0.005 * 4.2  
     return cacao_en_polvo, azucar, brandy_gramos, vainilla_gramos
 
-st.set_page_config(page_title="Calculadora de ingredientes para barra de chocolate amargo con brandy", page_icon=":chocolate_bar:", page_font_size=14)
+st.set_page_config(page_title="Calculadora de ingredientes para barra de chocolate amargo con brandy", page_icon=":chocolate_bar:")
 
-st.title("**Calculadora de ingredientes para barra de chocolate amargo con brandy**")
-
+st.markdown("<h1 style='text-align: center; font-weight: bold;'>Calculadora de ingredientes para barra de chocolate amargo con brandy</h1>", unsafe_allow_html=True)
 
 manteca_de_cacao = st.number_input("Cantidad de manteca de cacao (en gramos)", min_value=0.0, step=1.0, value=150.0)
 
@@ -34,6 +33,7 @@ csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()
 href = f'<a href="data:file/csv;base64,{b64}" download="receta_chocolate_amargo.csv">Descargar receta</a>'
 st.markdown(href, unsafe_allow_html=True)
+
 
 
 

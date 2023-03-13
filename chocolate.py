4cd3aa5228c86122cdac028cc00028d8c79c46d5
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-03-12 18:18:18 -0600

    Update chocolate.py

--- chocolate.py
+++ chocolate.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import base64
 
-
-
 def calculate_ingredients(manteca_de_cacao):
     cacao_en_polvo = manteca_de_cacao * 0.25
     azucar = manteca_de_cacao * 0.3
@@ -19,12 +17,12 @@ manteca_de_cacao = st.number_input("Cantidad de manteca de cacao (en gramos)", m
 
 cacao_en_polvo, azucar, brandy_gramos, vainilla_gramos = calculate_ingredients(manteca_de_cacao)
 
-def total=(manteca_de_cacao+cacao_en_polvo+azucar+brandy_gramos+vainilla_gramos)
+total = manteca_de_cacao + cacao_en_polvo + azucar + brandy_gramos + vainilla_gramos
 
 data = {
     "Ingrediente": ["Manteca de cacao", "Cacao en polvo sin azucar", "Azucar, jarabe o endulzante", "Brandy", "Extracto de vainilla", "TOTAL"],
     "Cantidad": [manteca_de_cacao, cacao_en_polvo, azucar, brandy_gramos, vainilla_gramos, total],
-    "Unidad": ["gramos", "gramos", "gramos", "gramos", "gramos"]
+    "Unidad": ["gramos", "gramos", "gramos", "gramos", "gramos", "gramos"]
 }
 
 df = pd.DataFrame(data)
@@ -35,6 +33,7 @@ csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()
 href = f'<a href="data:file/csv;base64,{b64}" download="receta_chocolate_amargo.csv">Descargar receta</a>'
 st.markdown(href, unsafe_allow_html=True)
+
 
 
 
